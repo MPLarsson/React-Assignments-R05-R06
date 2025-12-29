65335e305df788f4feef2009ed2c95f0c304f592
MPLarsson <Mattias.Larsson@stud.sundsgarden.se>
2025-12-29 16:14:18 +0100

    Refactored workflows

--- React-05-06/vite.config.js
+++ React-05-06/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Assignments-R05-R06/'
+  base: '/React-Assignments-R05-R06/',
+  build: {
+    outDir: 'docs'
+  }
 })
